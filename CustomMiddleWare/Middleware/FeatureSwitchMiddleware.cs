a4ad54900f4d59769bd185a3e7c8e24aa52abb32
Loganathan M <Loganathan_M3@Dellteam.com>
2021-06-12 10:27:53 +0530

    Added a custom middleware to intercept the requests

---
+++ CustomMiddleWare/Middleware/FeatureSwitchMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWare.Middleware
+{
+    public class FeatureSwitchMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public FeatureSwitchMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IConfiguration configuration)
+        {
+            if (httpContext.Request.Path.Value.Contains("/features"))
+            {
+                var switches = configuration.GetSection("FeatureSwitches");
+                var report = switches.GetChildren().Select(x => $"{x.Key} : {x.Value}");
+                await httpContext.Response.WriteAsync(string.Join("\n", report));
+            }
+
+            else
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+}
--- CustomMiddleWare/Startup.cs
+++ CustomMiddleWare/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomMiddleWare.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,7 @@ namespace CustomMiddleWare
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<FeatureSwitchMiddleware>();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
